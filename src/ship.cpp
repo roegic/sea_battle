f36ec5c2cba8e5805a9e6125a8be382d16579522
Elizaveta Slavinskaya <esslavinskaya@edu.hse.ru>
2023-12-10 13:45:28 +0300

    add ship

--- src/ship.cpp
+++ src/ship.cpp
@@ -1,3 +1,21 @@
-//
-// Created by 79116 on 10.12.2023.
-//
+#pragma once
+
+#include "../headers/ship.h"
+#include "string"
+
+Ship::Ship(int size, std::string type) : size_(size), type_(std::move(type)) {
+}
+
+bool Ship::IsSunk() const {
+    return hits_ >= size_;
+}
+
+int Ship::GetSize() const {
+    return size_;
+}
+
+void Ship::Hit() {
+    if (hits_ < size_) {
+        ++hits_;
+    }
+}
