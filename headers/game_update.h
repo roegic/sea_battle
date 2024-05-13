161e9a0da9b0a12cef831747269848660e95a238
vvsalash <vova.salash609@gmail.com>
2024-05-13 12:51:57 +0300

    game updated

--- headers/game_update.h
+++ headers/game_update.h
@@ -2,11 +2,12 @@
 
 #include "player.h"
 #include <iostream>
+#include "network.h"
 
 using namespace std;
 
-void AddShips (Player &player);
+void AddShips (Player &player, int type);
 void GetCoordinatesFire(Player &player1, Player &player2);
 void EnterToContinue();
-void GetCoordinates(char &xc, char &yc);
-void GetDirection(int &direction);
+void GetCoordinates(char &xc, char &yc, int type);
+void GetDirection(int &direction, int type);
