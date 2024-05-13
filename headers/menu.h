801cada76602b2e72cf0a62710d38a44c1ccb896
vvsalash <vova.salash609@gmail.com>
2024-05-13 12:46:23 +0300

    menu updated

--- headers/menu.h
+++ headers/menu.h
@@ -2,7 +2,9 @@
 
 #include <iostream>
 #include "Player.h"
+#include "network.h"
+using namespace std;
 
 void DrawTitle();
 
-void ShowMenu(Player &player1, Player &player2);
+void ShowMenu(Player &player1, Player &player2, char &type);
--- src/menu.cpp
+++ src/menu.cpp
@@ -6,41 +6,48 @@ void DrawTitle(){
     std::cout << "-------------------------------------------------\n";
 }
 
-void ShowMenu(Player &player1, Player &player2) {
+void ShowMenu(Player &player1, Player &player2, char& type) {
 
     std::cout << " 1. Start 2 Players Game"<< std::endl;
     std::cout << " 2. Start Game With Computer"<< std::endl;
-    std::cout << " 3. Exit Game\n"<< std::endl;
+    std::cout << " 3. Multiplayer"<< std::endl;
+    std::cout << " 4. Exit Game\n"<< std::endl;
 
     string command_type;
     string player_name;
 
     while (true) {
-        std::cout<< "Select option(type in console 1 or 3): ";
+        std::cout<< "Select option(type in console 1 or 4): ";
         getline(cin, command_type);
-        if(command_type == "1") { // пользователь нажал кнопку выход
+        if(command_type == "1") {
+            type = 'l';
             std::cout << "\x1B[2J\x1B[H" << std::endl;
             break;
         }  else if (command_type == "2") {
             std::cout << "\x1B[2J\x1B[H" << std::endl;
             std::cout << "\n Sorry, this feature is not implemented yet, right now option 1 works :)" << std::endl;
             exit(0);
-        }
-        else if (command_type == "3") {
+        } else if (command_type == "3") {
+            std::cout << "\x1B[2J\x1B[H" << std::endl;
+            type = create_connection();
+            break;
+        } else if (command_type == "4") {
             exit(0);
-        }else {
-            std::cout<< "Command is not correct. You should enter 1 or 3"<< std::endl;
+        } else {
+            std::cout<< "Command is not correct. You should enter 1 or 4"<< std::endl;
         }
     }
-    std::cout << " Enter player1 name: ";
-    getline(cin,player_name);
-    player1.SetPlayerName(player_name);
-
-    std::cout << "\n";
-    std::cout << "\x1B[2J\x1B[H" << std::endl; // эта магия отчищает консоль, но работает на OSX только
-    std::cout << " Enter player2 name: ";
-    getline(cin,player_name);
-    player2.SetPlayerName(player_name);
+//  std::cout << " Enter player1 name: ";
+//  getline(cin,player_name);
+//  player1.SetPlayerName(player_name);
+//
+//  std::cout << "\n";
+//  std::cout << "\x1B[2J\x1B[H" << std::endl; // эта магия отчищает консоль, но работает на OSX только
+//  std::cout << " Enter player2 name: ";
+//  getline(cin,player_name);
+//  player2.SetPlayerName(player_name);
+    player1.SetPlayerName("Player1");
+    player2.SetPlayerName("Player2");
 
     std::cout << "\n";
     std::cout << "\x1B[2J\x1B[H" << std::endl;
