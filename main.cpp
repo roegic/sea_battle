edaf7f761e8f84035060b470c8be404989340655
Elizaveta Slavinskaya <esslavinskaya@edu.hse.ru>
2024-05-13 13:47:44 +0300

    upgate main

--- main.cpp
+++ main.cpp
@@ -8,18 +8,25 @@
 using namespace std;
 
 int main() {
-  Player player1;
-  Player player2;
-  player1.CreateShip();
-  player2.CreateShip();
-  DrawTitle();
-  char type;
-  ShowMenu(player1, player2, type);
+    Player player1;
+    Player player2;
+    player1.CreateShip();
+    player2.CreateShip();
+    DrawTitle();
+    char type;
+    ShowMenu(player1, player2, type);
 
-  AddShips(player2, 1);
-  AddShips(player1, 1);
+    if (type == 'l') {
+        AddShips(player2, 1);
+        AddShips(player1, 1);
+    }
+    if (type == 'h') {
+        AddShips(player1, 2);
+    }
+    if (type == 'j') {
+        AddShips(player2, 3);
+    }
 
 
-
-  GetCoordinatesFire(player1, player2); // todo move it to addShip
+    GetCoordinatesFire(player1, player2); // todo move it to addShip
 }
--- src/game_update.cpp
+++ src/game_update.cpp
@@ -6,115 +6,123 @@
 #include <cctype>
 
 string invalid_input = "Invalid input! Please try again";
+vector<pair<int, int>> client_field;
+vector<int> client_directions;
+vector<pair<int, int>> server_field;
+vector<int> server_directions;
+int SHIPS_CNT = 10;
 
 void GetCoordinatesFire(Player &player1, Player &player2) {
-  int turn = 1;
-  while (true) {
-    Player *selectedPlayer;
-    Player *otherPlayer;
-    if (turn % 2 == 0){
-      selectedPlayer = &player1;
-      otherPlayer = &player2;
-    } else{
-      selectedPlayer = &player2;
-      otherPlayer = &player1;
-    }
-    if (player2.IsWin()){
-      cout << "\nPlayer " << player2.GetPlayerName() << " you won!" << endl;
-      break;
-    }
-    if(player1.IsWin()){
-      cout << "\nPlayer " << player1.GetPlayerName() << " you won!" << endl;
-      break;
-    }
-    cout << "\nTurn: " << turn << "\nPlayer " << selectedPlayer->GetPlayerName() << " enter coordinates to Fire \n \n";
-    otherPlayer->DrawHitFlield();
-
-    char xc;
-    char yc;
+    int turn = 1;
     while (true) {
-      GetCoordinates(xc, yc, 1);
-      int x = toupper(xc) - 'A';
-      int y = (yc - '0') - 1;
-      if(!otherPlayer->IsHitTwice(x, y)){
-        if(otherPlayer->FireEnemy(x, y)){
-          cout << "It's a Hit!"<<endl;
-            turn--;
-            selectedPlayer->DecHits();
-        }else{
-          cout << "It's a miss!"<<endl;
+        Player *selectedPlayer;
+        Player *otherPlayer;
+        if (turn % 2 == 0) {
+            selectedPlayer = &player1;
+            otherPlayer = &player2;
+        } else {
+            selectedPlayer = &player2;
+            otherPlayer = &player1;
         }
-        break;
-      }
-      else{
-        cout<<"You are trying shoot the same square twice, try again!"<<endl;
-      }
+        if (player2.IsWin()) {
+            cout << "\nPlayer " << player2.GetPlayerName() << " you won!" << endl;
+            break;
+        }
+        if (player1.IsWin()) {
+            cout << "\nPlayer " << player1.GetPlayerName() << " you won!" << endl;
+            break;
+        }
+        cout << "\nTurn: " << turn << "\nPlayer " << selectedPlayer->GetPlayerName()
+             << " enter coordinates to Fire \n \n";
+        otherPlayer->DrawHitFlield();
+
+        char xc;
+        char yc;
+        while (true) {
+            GetCoordinates(xc, yc, 1); // TODO ЭТО ПРОСТО СЕЙЧАС ЗАГЛУШКАА NEEDS FIXXX
+            int x = toupper(xc) - 'A';
+            int y = (yc - '0') - 1;
+            if (!otherPlayer->IsHitTwice(x, y)) {
+                if (otherPlayer->FireEnemy(x, y)) {
+                    cout << "It's a Hit!" << endl;
+                    turn--;
+                    selectedPlayer->DecHits();
+                } else {
+                    cout << "It's a miss!" << endl;
+                }
+                break;
+            } else {
+                cout << "You are trying shoot the same square twice, try again!"
+                     << endl;
+            }
+        }
+        cout << "\n";
+        EnterToContinue();
+        turn++;
     }
-    cout<<"\n";
-    EnterToContinue();
-    turn++;
-  }
-  cout<< "\n";
+    cout << "\n";
 }
 
 void GetDirection(int &direction, int type) {
-  char charDir;
-  std::vector<char> base = {'U','R','D','L'};
-  string tempDirection;
-  while (true) {
-    cout<< "Enter the direction (U/R/D/L): ";
-    getline(cin,tempDirection);
-    if (!isalpha(tempDirection[0]) || tempDirection.size() != 1) {
-      cout << invalid_input << endl;
-      continue;
+    char charDir;
+    std::vector<char> base = {'U', 'R', 'D', 'L'};
+    string tempDirection;
+    while (true) {
+        cout << "Enter the direction (U/R/D/L): ";
+        getline(cin, tempDirection);
+        if (!isalpha(tempDirection[0]) || tempDirection.size() != 1) {
+            cout << invalid_input << endl;
+            continue;
+        }
+        charDir = toupper(tempDirection[0]);
+        bool isValid = false;
+        for (int i = 0; i < base.size(); ++i) {
+            if (base[i] == charDir) {
+                direction = i + 1;
+                isValid = true;
+                break;
+            }
+        }
+        if (!isValid) {
+            cout << invalid_input << endl;
+        } else {
+            break;
+        }
     }
-    charDir = toupper(tempDirection[0]);
-    bool isValid = false;
-    for (int i = 0; i < base.size(); ++i){
-      if(base[i] == charDir){
-        direction = i + 1;
-        isValid = true;
-        break;
-      }
-    }
-    if (!isValid) {
-      cout << invalid_input << endl;
-    } else {
-      break;
-    }
-  }
 }
 
 
 void GetCoordinates(char &xc, char &yc, int type) {
-  while (true) {
-    cout << "Enter the coordinate [letter(A-H)][number(1-8)] (example: A1): ";
-    std::string xcord;
-    getline(cin,xcord);
-    string x_enter;
-    for (int i = 0; i < xcord.size(); ++i) {
-      if (!isspace(xcord[i])) {
-        x_enter += xcord[i];
-      }
+    while (true) {
+        cout << "Enter the coordinate [letter(A-H)][number(1-8)] (example: A1): ";
+        std::string xcord;
+        getline(cin,xcord);
+        string x_enter;
+        for (int i = 0; i < xcord.size(); ++i) {
+            if (!isspace(xcord[i])) {
+                x_enter += xcord[i];
+            }
+        }
+        if (x_enter.length() == 2) {
+            if (!isalpha(x_enter[0])) {
+                cout << invalid_input << endl;
+            } else if (!(toupper(x_enter[0]) - 'A' < 9 &&
+                         toupper(x_enter[0]) - 'A' >= 0)) {
+                cout << invalid_input << endl;
+            } else if (!isdigit(x_enter[1])) {
+                cout << invalid_input << endl;
+            } else if (x_enter[1] - '0' < 1 || x_enter[1] - '0' >= 9) {
+                cout << invalid_input << endl;
+            } else {
+                xc = toupper(x_enter[0]);
+                yc = x_enter[1];
+
+                break;
+            }
+        } else if (xcord != ""){
+            cout << invalid_input << endl;
+        }
     }
-    if (x_enter.length() == 2) {
-      if (!isalpha(x_enter[0])) {
-        cout << invalid_input << endl;
-      } else if (!(toupper(x_enter[0]) - 'A' < 9 && toupper(x_enter[0]) - 'A' >= 0)) {
-        cout << invalid_input << endl;
-      } else if (!isdigit(x_enter[1])) {
-        cout << invalid_input << endl;
-      } else if (x_enter[1] - '0' < 1 || x_enter[1] - '0' >= 9) {
-          cout << invalid_input << endl;
-      } else {
-        xc = toupper(x_enter[0]);
-        yc = x_enter[1];
-        break;
-      }
-    } else {
-      cout<< invalid_input<<endl;
-    }
-  }
 }
 
 void AddShips(Player &player, int type) {
@@ -143,7 +151,9 @@ void AddShips(Player &player, int type) {
         if (player.PlaceShip(x, y, direction, i)) {
           break;
         } else {
-          cout << "The location of the ships is incorrect! \nShips should not overlap, go beyond borders!\nPlease try again." << endl;
+            cout << "The location of the ships is incorrect! \nShips should not "
+                    "overlap, go beyond borders!\nPlease try again."
+                 << endl;
         }
       }
       cout << "\x1B[2J\x1B[H" << endl;
